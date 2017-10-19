9f88e7fd2972fd64375737b08106ff87e1415bad
CharlesClements <CharlesClements@gmail.com>
2017-10-19 10:04:22 -0400

    removed reddit files
    Not sure why I added them. May have been by mistake.

--- php/reddit/introducingphp/ch04/04_05/cdc.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Convert $total_minutes to hours and minutes.
-
-$total_minutes = 318;
-$minutes = $total_minutes % 60;
-
-$hours = floor( $total_minutes / 60 );
-
-echo "Time taken was $hours hours $minutes minutes";
